e47fb1af299044365dcd5761cd0047b40f1b3797
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-08-24 11:00:56 -0700

    remove logging messages

--- pi_sim.R
+++ pi_sim.R
@@ -2,12 +2,10 @@
 
 sim.pi <- function() {
     iterations = iters_per_slave
-    print("generating vectors")
     x.pos <- runif(iterations, min=-1, max=1)
     y.pos <- runif(iterations, min=-1, max=1)
     # Test if draws are inside the unit circle
     draw.pos <- ifelse(x.pos^2 + y.pos^2 <= 1, TRUE, FALSE)
     draws.in <- length(which(draw.pos == TRUE))
-    print("generating vectors")
     return(draws.in)
 }
